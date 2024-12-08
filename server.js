02ac2f33e6f37926b307c0c55faa8227a7d85963
Wesley Soeiro <140848246+WesleySoeiro@users.noreply.github.com>
2024-12-08 15:55:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
-
+const teste = "alterando"
 
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
